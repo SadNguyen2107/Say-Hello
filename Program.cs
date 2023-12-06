bc49a583fb32b331e5b9aa496116cb958281e7db
Sad <nguyentrungson217@gmail.com>
2023-12-06 21:27:36 +0700

    HomeWork Week2

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Program
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+            String? name = Console.ReadLine();
+
+            Console.WriteLine($"Hello {name}");
+        }
+    }
+}
